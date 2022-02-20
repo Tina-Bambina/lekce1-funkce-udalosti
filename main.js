88b0b99d4af75172ec80b50a5871210a4d2c34e7
Tina-Bambina <speleerska@gmail.com>
2022-02-20 15:48:16 +0400

    function upozorni

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
-
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
  *
@@ -18,12 +16,23 @@ function secti(a, b) {
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
-  alert("Gratulace, právě jsi spustila tuto funkci!");
-  console.log("#vysledek");
+  alert('Gratulace, právě jsi spustila tuto funkci!');
+  console.log('Gratulace, právě jsi spustila tuto funkci!');
+  let ctv =  document.getElementsByClass('.ctverecek')[0].
+  ctv.innerHTML = 'Gratulace, konecne'; //nemeni mi to text ctverecku a uz opravdu nevim proc
+  
+  //potom by nasledovalo: 
+  //ctv.style.width = '400px';
 }
 
 function zmenBarvu()
 {
   document.querySelector('.ctverecek').style.backgroundColor = 'green';
+  console.log('Zmenili jsme barvu ctverecku');
 }
 
+function zobrazVysledek()
+{
+  document.querySelector("#vysledek").innerHTML = secti(4,5);
+  console.log('Nas vysledek: ' + secti(4,5))
+}
